cddc1471959b73eae4fe7ee19b0c864643de5a37
gustavo.rodriguez@globallogic.com <gustavo.rodriguez@globallogic.com>
2014-04-10 20:54:24 -0300

    Correciones al modelo de dominio

--- Bugzzinga.Dominio/Proyecto.cs
+++ Bugzzinga.Dominio/Proyecto.cs
@@ -14,10 +14,16 @@ namespace Bugzzinga.Dominio
         private IList<Usuario> _miembros = new ArrayList4<Usuario>();
         private IList<TipoItem> _tiposDeItem = new ArrayList4<TipoItem>();
         private IList<Item> _items =  new ArrayList4<Item>();
+        private ArrayList4<Prioridad> _prioridades = new ArrayList4<Prioridad>();
 
         public IEnumerable<Usuario> Miembros { get { return this._miembros; } }
         public IEnumerable<TipoItem> TiposDeItem { get {return this._tiposDeItem; } }
         public IEnumerable<Item> Items { get { return this._items; } }
+        public ArrayList4<Prioridad> Prioridades
+        {
+            get { return this._prioridades; }
+        }
+
 
         public string Codigo { get; set; }
         public string Nombre { get; set; }
