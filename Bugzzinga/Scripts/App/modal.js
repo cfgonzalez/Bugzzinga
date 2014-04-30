bd541146cc5a2335103a317a9ab79bb0f1bbbf34
gustavo.rodriguez@globallogic.com <gustavo.rodriguez@globallogic.com>
2014-04-30 00:08:13 -0300

    Componente Bootstrap-Angular para el uso de ventanas modales.
    Permite manejar el comportamiento de la vista (Grilla->Ventana Modal) de forma genérica para cualquier tipo de colección. De esta manera se garantiza reutilización y un comportamiento homogeneo del Frontend.

---
+++ Bugzzinga/Scripts/App/modal.js
@@ -0,0 +1,72 @@
+﻿//Directiva
+bugzzinga.directive('modalPopup', function () {
+    
+    return {
+        restrict: 'E',
+        controller: modalCtrl,
+        
+        //Vincula una propiedad del scope parent con el valor del atributo 'template' de la directiva
+        link: function (scope, element, attrs) {
+            scope.modalTemplate = attrs.template;
+        },
+        
+        //Toma el valor del atributo 'template' y lo setea como templateUrl
+        templateUrl: function ($node, attrs) {
+            return attrs.template;
+        }
+    };
+});
+
+//Controller del Modal
+var modalCtrl = function ($scope, $modal, $http) {
+
+    $scope.open = function () {
+
+        //Filtra la colección de usuarios según el Código elegido en la grilla.
+        var entidadesSeleccionadas = $.grep($scope.coleccion, function (entidad) { return entidad.Codigo == $scope.codigoEntidadSeleccionada; });
+
+        var modalInstance = $modal.open({
+            templateUrl: $scope.modalTemplate,
+            controller: ModalInstanciaCtrl,
+            resolve: {
+                //Sucede antes de abrir el popup. Devuelve el objeto que se inyecta en ModalInstanciaCtrl
+                entidadSeleccionada: function () {
+                    return entidadesSeleccionadas[0];
+                }
+            }
+        });
+
+        //Sucede después de aceptar el popup
+        modalInstance.result.then(function (entidad) {
+            $http.post($scope.servicioPersistencia,
+                $.param(entidad),
+                {
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+                }
+            ).success(function (response) {
+
+                //Actualiza la referencia de la entidad modificada, para que te actualice el binding y se refresque la grilla
+                var entidadesModificadas = $.grep($scope.coleccion, function (entidad) { return entidad.Codigo == response.Codigo; });
+                entidadesModificadas[0] = response;
+            });
+        });
+    };
+};
+
+//Sucede antes de abrir el popup
+var ModalInstanciaCtrl = function ($scope, $modalInstance, entidadSeleccionada) {
+
+    //Setea el scope que se bindeará al template
+    $scope.entidadSeleccionada = entidadSeleccionada;
+
+    $scope.ok = function () {
+        $modalInstance.close(entidadSeleccionada);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
+
+
+
