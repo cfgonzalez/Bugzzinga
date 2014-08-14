49d97bb63fcd6ceca9b4db9afa33318426a55c77
Gustavo Rodriguez <gustavo.rodriguez@globallogic.com>
2014-08-14 15:10:46 -0300

    Frontend completo de ABM de Perfiles.

---
+++ Bugzzinga/Client/App/Common/servicios.js
@@ -0,0 +1,10 @@
+﻿var usuarioServicio = bugzzinga.factory('usuarioServicio', function ($resource) {
+
+    return $resource('/api/Usuarios/:id', { id: '@id' }, { update: { method: 'PUT' }, add: { method: 'POST' }, remove: { method: 'DELETE' } });
+
+});
+
+var perfilServicio = bugzzinga.factory('perfilServicio', function ($resource) {
+
+    return $resource('/api/Perfiles/:id', { id: '@id' }, { update: { method: 'PUT' }, add: { method: 'POST' }, remove: { method: 'DELETE' } });
+});
