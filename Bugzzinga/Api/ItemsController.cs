d2b532cbfa29f304e0614f645ca953234cc38807
Gustavo Rodriguez <gusrodryguez@gmail.com>
2014-09-12 20:43:51 -0300

    Archivos que quedaron fuera del staging y no se subieron

---
+++ Bugzzinga/Api/ItemsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Bugzzinga.Dominio;
+
+namespace Bugzzinga.Api
+{
+    public class ItemsController : ApiController
+    {
+        //Trae los usuarios para un proyecto 
+        public IEnumerable<Item> Get()
+        {
+            return TraerListaItemsDummy(); ;
+        }
+
+        //Trae los usuarios para un proyecto 
+        public IEnumerable<Item> Get(int codigoProyecto)
+        {
+            //Devuelve una sublista dummy del total de usuarios
+            var lista = TraerListaItemsDummy();
+
+            lista.RemoveAt(1);
+
+            return lista;
+        }
+
+        private List<Item> TraerListaItemsDummy()
+        {
+            var listaItems = new List<Item>();
+
+            for (var i = 0; i < 3; i++)
+            {
+                var tipo = new TipoItem("t1", "TipoItem1");
+
+                var prioridad = new Prioridad("P1", "Prioridad 1");
+
+                var item = new Item("Nombre" + (i + 1).ToString(), "Descripcion" + (i + 1).ToString(), tipo, prioridad);
+
+                item.Responsable = new Usuario();
+
+                item.Responsable.Codigo = 2;
+
+                item.Codigo = (i + 1);
+                
+                listaItems.Add(item);
+            }
+
+            return listaItems;
+        }
+    }
+}
---
+++ Bugzzinga/Api/PrioridadesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Bugzzinga.Dominio;
+
+namespace Bugzzinga.Api
+{
+    public class PrioridadesController : ApiController
+    {
+        //Trae los usuarios para un proyecto 
+        public IEnumerable<Prioridad> Get()
+        {
+            //Devuelve una sublista dummy del total de prioridades
+            var lista = TraerListaPrioridadesDummy();
+
+            return lista;
+        }
+
+        private List<Prioridad> TraerListaPrioridadesDummy()
+        {
+            var listaPrioridades = new List<Prioridad>();
+
+            for (var i = 1; i < 8; i++)
+            {
+                listaPrioridades.Add(new Prioridad("P" + i.ToString(), "Prioridad" + i.ToString()));
+            }
+
+            return listaPrioridades;
+        }
+    }
+}
+
