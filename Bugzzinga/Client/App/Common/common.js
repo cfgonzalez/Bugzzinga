176690875b2b377f40990226ab212d5e9afa49b9
Gustavo Rodriguez <gusrodryguez@gmail.com>
2014-12-02 23:06:28 -0300

    Se agregó el div que anula la UI durante el request.

--- Bugzzinga/Client/App/Common/common.js
+++ Bugzzinga/Client/App/Common/common.js
@@ -1,4 +1,13 @@
-﻿//Mergea dos colecciones por la propiedad de un objeto
+﻿Pace.on("start", function () {
+    $("#paceDiv").css("display", "block");
+});
+
+Pace.on("done", function () {
+    $("#paceDiv").css("display", "none");
+});
+
+
+//Mergea dos colecciones por la propiedad de un objeto
 function mergearColeccionPorPropiedad(arr1, arr2, prop) {
 
     return _.each(arr2, function (arr2obj) {
