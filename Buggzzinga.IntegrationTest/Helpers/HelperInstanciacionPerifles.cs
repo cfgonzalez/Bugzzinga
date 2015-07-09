9243dc63e6d7d02698b8385ab98c715b828bcace
Cristian Gonzalez <cfgonzalez@gmail.com>
2015-07-09 18:24:26 -0300

    Se agrega un listado de tests para el controller de proyectos. Siempre hay que correr los listados ya que en caso contrario los tests fallan cuando vs los corre en paralelo (claro, ya que en realidad deberian ser tests unitarios)

---
+++ Buggzzinga.IntegrationTest/Helpers/HelperInstanciacionPerifles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bugzzinga.Dominio;
+
+namespace Buggzzinga.IntegrationTest.Helpers
+{
+    public class HelperInstanciacionPerifles
+    {
+
+        public static List<Perfil> GetProyectos( int cantidad )
+        {
+            List<Perfil> resultado = new List<Perfil>();
+
+            for ( int i = 1; i <= cantidad; i++ )
+            {
+                Perfil p = new Perfil();                
+                p.Descripcion = String.Concat( "Perfil de prueba ", i );
+                p.Nombre = String.Concat( "Perfil ", i );
+
+                resultado.Add( p );
+            }
+
+            return resultado;
+        }
+    }
+}
